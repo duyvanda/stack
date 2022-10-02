6938b99b84a8fcf1051cf4a61db25cbca3d70790
duyvanda <68499531+duyvanda@users.noreply.github.com>
2022-10-02 19:21:15 +0700

    Update sync_dms_ardoc.py

--- dags/sync_dms_ardoc.py
+++ dags/sync_dms_ardoc.py
@@ -204,11 +204,13 @@ def update_sync_dms_ardoc():
 
 dummy_start = DummyOperator(task_id="dummy_start", dag=dag)
 
+dummy_end = DummyOperator(task_id="dummy_end", dag=dag)
+
 insert = PythonOperator(task_id="insert", python_callable=insert, dag=dag)
 
 update = PythonOperator(task_id="update", python_callable=update, dag=dag)
 
 update_sync_dms_ardoc = PythonOperator(task_id="update_sync_dms_ardoc", python_callable=update_sync_dms_ardoc, dag=dag)
 
-dummy_start >> update >> insert >> update_sync_dms_ardoc
+dummy_start >> update >> insert >> update_sync_dms_ardoc >> dummy_end
 
