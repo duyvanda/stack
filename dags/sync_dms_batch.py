d6caef06ebe425383253c13feb8ac66bb29cda8a
duyvanda <68499531+duyvanda@users.noreply.github.com>
2022-10-02 19:32:36 +0700

    Update sync_dms_batch.py

--- dags/sync_dms_batch.py
+++ dags/sync_dms_batch.py
@@ -192,7 +192,7 @@ update = PythonOperator(task_id="update", python_callable=update, dag=dag)
 
 update_sync_dms_ardoc_2 = PythonOperator(task_id="update_sync_dms_ardoc_2", python_callable=update_sync_dms_ardoc_2, dag=dag)
 
-dummy_start >> update >> insert >> update_sync_dms_ardoc_2 dummy_end
+dummy_start >> update >> insert >> update_sync_dms_ardoc_2 >> dummy_end
 
 # print(table_name, table_temp, datenow_mns45)
 
